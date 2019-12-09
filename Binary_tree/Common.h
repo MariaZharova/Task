2197825e626e15bf935d32902ecfd7444b9e5512
MariaZharova <57617173+MariaZharova@users.noreply.github.com>
2019-12-09 22:36:01 +0300

    The file common.h was added
    The file was fixed according to the addition of a separate file with preprocessor directives (common .h)

---
+++ Binary_tree/Common.h
@@ -0,0 +1,17 @@
+/*
+ * File:  common.h
+ * Brief: Common definitions for file Binary_tree.c
+ * Author: Maria Zharova
+ * Created on 09.12.2019
+ * (c) MIPT 2019
+ */
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Binary_tree.h"
+
+#define ERROR_OUT_OF_MEMORY NULL -1
+#define ERROR_INVALID_INPUT NULL -2
+#define ERROR_INVALID_INPUT_INT -3
