755a1a6dcaa899db3b4c49ae6f18c4226e17166b
MariaZharova <57617173+MariaZharova@users.noreply.github.com>
2019-12-09 22:15:59 +0300

    Common.h was added
    This is a separate file with preprocessor directives

---
+++ Single_List/Common.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Sl_list.h"
+
+#define ERROR_OUT_OF_MEMORY NULL
+#define ERROR_INVALID_INPUT NULL
+#define ERROR_INVALID_INPUT_INT -1
+
