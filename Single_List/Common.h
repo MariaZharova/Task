70dc41cce7397549ddeee8382241ce458566e8d5
MariaZharova <57617173+MariaZharova@users.noreply.github.com>
2019-12-09 22:23:12 +0300

    Update Common.h
    File description was added

--- Single_List/Common.h
+++ Single_List/Common.h
@@ -1,3 +1,11 @@
+/*
+ * File:  common.h
+ * Brief: Common definitions for file sl_list.c
+ * Author: Maria Zharova
+ * Created on 09.12.2019
+ * (c) MIPT 2019
+ */
+
 #pragma once
 
 #include <stdio.h>
