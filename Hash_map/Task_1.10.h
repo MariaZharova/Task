f02fc41d7058e381906253087737060431ca1a73
MariaZharova <57617173+MariaZharova@users.noreply.github.com>
2019-12-14 15:48:40 +0300

    Added file Task_1.10.h
    This is header-file for solution Task № 1.10 from zadavalnik

---
+++ Hash_map/Task_1.10.h
@@ -0,0 +1,26 @@
+/*
+ * File:  task_1.10.c
+ * Brief: header file for task 1.10 from zadavalnik
+ * Author: Maria Zharova <MZharovaNN@yandex.ru>
+ * Created on 14.12.2019
+ * (c) MIPT 2019
+ */
+
+#pragma once
+
+ /*
+ * Insert new name and number into table
+ * Parameters
+ *    name - pointer to the person's name
+ *    number - value of pearson's phone number
+ * Returns none
+ */
+void INSERT(char* name, unsigned long long number);
+
+/*
+* Find name in the table
+* Parameters
+*    name - pointer to the person's name
+* Returns none
+*/
+void FIND(char* name);
