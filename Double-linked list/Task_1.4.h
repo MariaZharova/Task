368b3ed88b4d56ea9dfa41512e18f1b9b326d537
MariaZharova <57617173+MariaZharova@users.noreply.github.com>
2019-12-14 03:10:21 +0300

    Addede file Task_1.4.h
    This is header-file for solution task № 1.4 from zadavalnik

---
+++ Double-linked list/Task_1.4.h
@@ -0,0 +1,17 @@
+/*
+ * File:  task_1.4.h
+ * Brief: heaser-file for function brackets (task 1.4)
+ * Author: Maria Zharova <MZharovaNN@yandex.ru>
+ * Created on 10.12.2019
+ * (c) MIPT 2019
+ */
+
+#pragma once
+
+ /*
+ * Checking the sequence for relation to the correct bracket expression
+ * Params 
+ *     pointer to the expression
+ * Returns 10 in case of correct expression, -10 in case of incorrect expression
+ */
+int brackets(char *str);
