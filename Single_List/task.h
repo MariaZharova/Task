e0ad1fea7d6ced454525cb4078be72ba07f7cdd5
2hard4me <57602913+2hard4me@users.noreply.github.com>
2019-12-15 17:29:07 +0300

    Add file task.h

---
+++ Single_List/task.h
@@ -0,0 +1,17 @@
+/*
+* This file is task.h
+* Single linked list task header file
+* Author Abubakr Usmanov <usmanov.aa@phystech.edu>
+* Created 07.12.2019
+*/
+
+
+#include "Common.h"
+
+plist first_N_integers(int N);
+/*
+Function:		first_N_integers
+Description:	creates list of numbers from 1 to N
+Arguments:		int N;
+Returning value:list
+*/
