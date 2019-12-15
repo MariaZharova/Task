6cb1502b7121f250062e689bf9197f33ce2765ef
2hard4me <57602913+2hard4me@users.noreply.github.com>
2019-12-15 20:49:02 +0300

    Update SL_list_test.c

--- Single_List/SL_list_test.c
+++ Single_List/SL_list_test.c
@@ -5,13 +5,7 @@
 * Created 07.12.2019
 */
 
-#include <stdio.h>
-#include "Sl_list.h"
-
-#define KRED "\x1B[31m" // red
-#define KGRN "\x1B[32m" // green
-#define KWHT "\x1B[37m" // white
-#define KBLK "\x1B[30m" // black
+#include"Common.h"
 
 int test1()
 {
@@ -20,50 +14,44 @@ int test1()
 	list = create_list();
 	printf("List created! %p\n", list);
 	insert_entry(list, 1);
-	insert_entry(list, 2);
+	insert_entry(list, 2);	
 	insert_entry(list, 3);
 	print_list(list);
 
-	destroy_list(list);
+	int k = destroy_list(list);
 	return 0;
 }
 
 int test2()
 {
-	plist list = create_list();
+	plist list1 = create_list();
 
-	if (NULL != list->head)
-		goto error_path;
-	if (0 != list->size)
-		goto error_path;
+	if (NULL != list1->head)
+	{
+		int k = destroy_list(list1);
+		return -1;
+	}
+	if (0 != list1->size)
+	{
+		int k = destroy_list(list1);
+		return -1;
+	}
+	list1 = first_N_integers(4);
+	int k = destroy_list(list1);
 
-	destroy_list(list);
 	return 0;
-error_path:
-	destroy_list(list);
-	return -1;
 }
 
 int test3()
 {
-	plist list = create_list();
+	plist list2 = create_list();
 
-	insert_entry(list, 1);
-	insert_entry(list, 1);
-	insert_entry(list, 2);
-	delete_entry(list, 1);
+	insert_entry(list2, 1);
+	insert_entry(list2, 1);
+	insert_entry(list2, 2);
+	delete_entry(list2, 1);
 
-	int result = (list->size != 1) ? -1 : 0;
-	destroy_list(list);
+	int result = (list2->size != 1) ? -1 : 0;
+	int k = destroy_list(list2);
 	return result;
-}
-
-int main(void)
-{
-	printf("Hello, " "world!" "\n");
-	test1();
-	printf("TEST #2 Create list:\t\t%s%s\n", (test2() == 0) ? KGRN"PASSED" : KRED"FAILED", KWHT); // or KBLK
-	printf("TEST #3 Remove element:\t\t%s%s\n", (test3() == 0) ? KGRN"PASSED" : KRED"FAILED", KWHT);
-
-	return 0;
 }
