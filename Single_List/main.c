7ce08c79a4d26d61a896ebf8aba6862ba1c311fe
2hard4me <57602913+2hard4me@users.noreply.github.com>
2019-12-15 20:48:34 +0300

    Update main.c

--- Single_List/main.c
+++ Single_List/main.c
@@ -12,13 +12,7 @@ int main(void)
 {
 	printf("Hello, " "world!" "\n");
 	test1();
-	printf("TEST #2 Create list:\t\t%s%s\n", (test2() == 0) ? KGRN"PASSED" : KRED"FAILED", KWHT); // or KBLK
-	printf("TEST #3 Remove element:\t\t%s%s\n", (test3() == 0) ? KGRN"PASSED" : KRED"FAILED", KWHT);
-
-	plist list;
-	list = first_N_integers(5);
-	print_list(list);
-	destroy_list(list);
-
+	printf("TEST #2 Create list:\t\t%s\n", (test2() == 0) ? "PASSED" : "FAILED");
+	printf("TEST #3 Remove element:\t\t%s\n", (test3() == 0) ? "PASSED" : "FAILED");
 	return 0;
 }
