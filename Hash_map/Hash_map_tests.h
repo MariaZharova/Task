9eaf8dab893fdb61f9a8bba2f5a94f414f0e2013
MariaZharova <57617173+MariaZharova@users.noreply.github.com>
2019-12-15 13:15:25 +0300

    Update Hash_map_tests.h
    Changed names of two last test-functions

--- Hash_map/Hash_map_tests.h
+++ Hash_map/Hash_map_tests.h
@@ -13,28 +13,28 @@ typedef int(*TEST)(void);
 /*
 * Two next functions for colorful messages
 */
-void init_tests();
-void deinit_tests();
+void init_tests(void);
+void deinit_tests(void);
 
 /*
 * Function for run all tests
 */
-int run_all_tests();
+int run_all_tests(void);
 
 /*
-* Test for function hash_map_init and hash_map_deinit
+* Test for function hash_map_deinit
 */
-int test_init_deinit()
+int test_deinit(void)
 
 /*
 * Test for function add and find2
 */
-int test_add_find2()
+int test_add_find(void)
 
 /*
 * Test for function del and find1
 */
-int test_del_find1()
+int test_del_find(void)
 
 
 
